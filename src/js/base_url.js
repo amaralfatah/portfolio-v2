ace2608aaca87de0393763f313cd884cbf5af131
amaralfatah <amaralfatah.main@gmail.com>
2024-12-01 12:24:13 +0700

    fix

--- src/js/base_url.js
+++ src/js/base_url.js
@@ -3,7 +3,7 @@ const baseUrl =
     ? "http://127.0.0.1:5500/"
     : "https://amar.my.id/portfolio-v2/";
 
-// Update all elements (links, stylesheets, scripts, and images) based on base URL
+// Fungsi untuk memperbarui semua URL
 const updateUrls = (selector, attr = "href") => {
   document.querySelectorAll(selector).forEach((el) => {
     const originalHref = el.getAttribute(`data-${attr}`);
@@ -11,8 +11,10 @@ const updateUrls = (selector, attr = "href") => {
   });
 };
 
+// Perbarui semua elemen dengan atribut URL
 updateUrls('link[rel="stylesheet"]', "href");
 updateUrls("script[data-src]", "src");
 updateUrls("footer a[data-href]");
 updateUrls("header a[data-href]");
 updateUrls('link[rel="icon"][data-href]', "href");
+updateUrls("img[data-src]", "src"); // Tambahan untuk elemen gambar
